f4b25bc3715f8d48cbae9dbd873da305669d064a
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-02-25 11:13:08 -0300

    Utilizando o dotenv para ler as variáveis de ambiente.

--- src/index.php
+++ src/index.php
@@ -1,3 +1,17 @@
 <?php
+require_once '../vendor/autoload.php';
 
-echo "Olá Mundo!!!";
+use Dotenv\Dotenv;
+
+echo "<pre>";
+echo "Olá Mundo!!!\n";
+
+var_dump(__DIR__);
+
+$dotenv = Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+print_r($_ENV);
+
+echo "\nNome do Banco de Dados: $_ENV[DB_NAME]";
+echo "\nServidor do Banco de Dados: $_ENV[DB_HOST]";
