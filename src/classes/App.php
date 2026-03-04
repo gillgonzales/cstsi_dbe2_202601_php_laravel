04d4313900707e91d8bfc3fd4b437a3e533f8ccd
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-04 12:10:20 -0300

    fix: carrega variáveis de ambiente da env na classe App

--- src/classes/App.php
+++ src/classes/App.php
@@ -2,11 +2,21 @@
 
 namespace CSTSI\Dbe2\classes;
 
-class App {
+use Dotenv\Dotenv;
 
-    public static function loadEnv(){
+class App
+{
+
+    public static function loadEnv()
+    {
         //TODO: Carregar as variáveis de ambiente aqui
         echo "\nCarregar ENV";
-    }
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
 
+        print_r($_ENV);
+
+        echo "\nNome do Banco de Dados: $_ENV[DB_NAME]";
+        echo "\nServidor do Banco de Dados: $_ENV[DB_HOST]";
+    }
 }
--- src/index.php
+++ src/index.php
@@ -2,20 +2,10 @@
 require_once '../vendor/autoload.php';
 
 use CSTSI\Dbe2\classes\App;
-use Dotenv\Dotenv;
-
 
 echo "<pre>";
 echo "Olá Mundo!!!\n";
 
 var_dump(__DIR__);
-
-$dotenv = Dotenv::createImmutable(__DIR__.'/../');
-$dotenv->load();
-
-print_r($_ENV);
-
-echo "\nNome do Banco de Dados: $_ENV[DB_NAME]";
-echo "\nServidor do Banco de Dados: $_ENV[DB_HOST]";
 
 App::loadEnv();
