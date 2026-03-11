414fd65b0abec9351773f909060a12fd6da60d04
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-10 23:31:32 -0300

    refactor: implementa métodos de CRUD no controller de Produtos

--- src/app/controllers/ProdutoController.php
+++ src/app/controllers/ProdutoController.php
@@ -2,22 +2,38 @@
 
 namespace CSTSI\Dbe2\app\controllers;
 
-use CSTSI\Dbe2\app\models\Produto;
+use CSTSI\Dbe2\app\models\{Produto, ProdutoDAO};
 
 class ProdutoController extends Controller
 {
-
     public function __construct()
     {
         parent::__construct();
-        $this->model = new Produto();
+        $this->model = new ProdutoDAO();
     }
 
     public function index()
     {
-        $listProdutos = $this->model->getAll();
+        $listProdutos = $this->model->read();
         $this->view->load('produtos/index', [
             'produtos' => $listProdutos
         ]);
+    }
+
+    public function create(){
+        $produto = new Produto(null,'teste','teste',12,123);
+        $this->model->create($produto);
+        return header("Location: /produtos");
+    }
+
+    public function update($id){
+        $produto = new Produto($id,'update','update',12,123);
+        $this->model->update($produto);
+        return header("Location: /produtos");
+    }
+
+    public function remove(int $id){
+        $this->model->delete($id);
+        return header("Location: /produtos");
     }
 }
