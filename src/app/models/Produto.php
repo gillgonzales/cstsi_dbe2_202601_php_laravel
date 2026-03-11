d1141f4d7364e188b0a04e7933948729b9a4eed9
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-10 23:29:40 -0300

    refactor: modifica produto para representar um modelo. Separa o modelo da lógica de acesso aos dados (ProdutoDAO)

--- src/app/models/Produto.php
+++ src/app/models/Produto.php
@@ -2,27 +2,106 @@
 
 namespace CSTSI\Dbe2\app\models;
 
-use Exception;
-
-class Produto extends Model
+class Produto
 {
+	private ?int $id_prod;
+	private string $nome;
+	private string $descricao;
+	private int $qtd_estoque;
+	private float $preco;
+	private bool $importado;
 
-    public function __construct()
-    {
-        $this->table = 'produtos';
-        $this->init();
-    }
+	public function __construct(
+		?int $id_prod = null,
+		string $nome = '',
+		string $descricao = '',
+		int $qtd_estoque = 0,
+		float $preco = 0.0,
+		bool $importado = false
+	) {
 
-    public function getAll(): array
-    {
-        $sql = "SELECT * FROM $this->table";
-        $prepStmt = $this->conn->prepare($sql);
+		$this->id_prod = $id_prod;
+		$this->nome = $nome;
+		$this->descricao = $descricao;
+		$this->qtd_estoque = $qtd_estoque;
+		$this->preco = $preco;
+		$this->importado = $importado;
+	}
 
-        if ($prepStmt->execute()) {
-            // var_dump($prepStmt);
-            return $prepStmt->fetchAll(self::FETCH);
-        } else {
-            throw new Exception("Erro ao ler dados do banco!");
-        }
-    }
+	public function getIdProd(): ?int
+	{
+		return $this->id_prod;
+	}
+
+	public function setIdProd(?int $id_prod): self
+	{
+		$this->id_prod = $id_prod;
+		return $this;
+	}
+
+	public function getNome(): string
+	{
+		return $this->nome;
+	}
+
+	public function setNome(string $nome): self
+	{
+		$this->nome = $nome;
+		return $this;
+	}
+
+	public function getDescricao(): string
+	{
+		return $this->descricao;
+	}
+
+	public function setDescricao(string $descricao): self
+	{
+		$this->descricao = $descricao;
+		return $this;
+	}
+
+	public function getQtdEstoque(): int
+	{
+		return $this->qtd_estoque;
+	}
+
+	public function setQtdEstoque(int $qtd_estoque): self
+	{
+		$this->qtd_estoque = $qtd_estoque;
+		return $this;
+	}
+
+	public function getPreco(): float
+	{
+		return $this->preco;
+	}
+
+	public function setPreco(float $preco): self
+	{
+		$this->preco = $preco;
+		return $this;
+	}
+
+	public function isImportado(): bool
+	{
+		return $this->importado;
+	}
+
+	public function setImportado(bool $importado): self
+	{
+		$this->importado = $importado;
+		return $this;
+	}
+
+	public function __get($name)
+	{
+		return $this->$name;
+	}
+
+	public function __set($name, $value)
+	{
+		$this->$name=$value;
+	}
+
 }
