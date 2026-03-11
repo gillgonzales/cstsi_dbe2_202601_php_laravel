1ee62ddc50306d59a12ea94b2aa600e96d5a1a99
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-10 23:28:33 -0300

    feat: cria a classe que implementa iDAO para produto e extende a Model

---
+++ src/app/models/ProdutoDAO.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace CSTSI\Dbe2\app\models;
+
+use CSTSI\Dbe2\app\interfaces\iDAO;
+use Exception;
+
+class ProdutoDAO extends Model implements iDAO
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->table = 'produtos';
+        $this->primaryKey = 'id_prod';
+    }
+
+    public function create(object $produto): bool
+    {
+        try {
+            $this->setValues($produto);
+            $this->values[":importado"] = $produto->importado ? 1 : 0;
+            if (!$this->insert())
+                throw new Exception("Erro ao inserir em $this->table!!");
+            return true;
+        } catch (\Exception $error) {
+            error_log("ERRO: " . print_r($error->getMessage(), TRUE));
+            $this->prepStmt ?? $this->dumpQuery($this->prepStmt);
+            var_dump($error);
+            var_dump($this->prepStmt);
+            return false;
+        } finally {
+            $this->dumpQuery($this->prepStmt);
+        }
+    }
+
+    public function read(int | null $id = null): array | bool
+    {
+        $sql = "SELECT * FROM $this->table";
+        $prepStmt = $this->conn->prepare($sql);
+
+        if ($prepStmt->execute()) {
+            // var_dump($prepStmt);
+            return $prepStmt->fetchAll(self::FETCH);
+        } else {
+            throw new Exception("Erro ao ler dados do banco!");
+        }
+    }
+
+    public function update(object $produto): bool
+    {
+        try {
+            $this->setValues($produto);
+            $this->values[":importado"] = $produto->importado ? 1 : 0;
+            $this->values[":id"] = $produto->id_prod;
+            if (!$this->updates())
+                throw new Exception("Erro ao atualizar em $this->table!!");
+            return true;
+        } catch (\Exception $error) {
+            echo "<pre>";
+            error_log("ERRO: " . print_r($error->getMessage(), TRUE));
+            $this->prepStmt ?? $this->dumpQuery($this->prepStmt);
+            var_dump($error);
+            var_dump($this->prepStmt);
+            return false;
+        } finally {
+            $this->dumpQuery($this->prepStmt);
+        }
+    }
+    public function delete(int $id): bool
+    {
+         try {
+            $this->values[":id"] = $id;
+            if (!$this->destroy())
+                throw new Exception("Erro ao remover em $this->table!!");
+            return true;
+        } catch (\Exception $error) {
+            echo "<pre>";
+            error_log("ERRO: " . print_r($error->getMessage(), TRUE));
+            $this->prepStmt ?? $this->dumpQuery($this->prepStmt);
+            var_dump($error);
+            var_dump($this->prepStmt);
+            return false;
+        } finally {
+            $this->dumpQuery($this->prepStmt);
+        }
+    }
+
+    protected function setColumns(): array
+    {
+        return $this->columns = [
+            'nome',
+            'descricao',
+            'qtd_estoque',
+            'preco',
+            'importado'
+        ];
+    }
+}
