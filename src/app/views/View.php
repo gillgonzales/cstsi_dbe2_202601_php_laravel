7fc8f2d77a6daecd70a25df2e9e614f29cc9de39
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-11 09:50:06 -0300

    fix: adiciona o type hint para string no parâmetro page

--- src/app/views/View.php
+++ src/app/views/View.php
@@ -4,7 +4,7 @@ namespace CSTSI\Dbe2\app\views;
 
 class View{
 
-    public static function load($page , array | null $data =null) : void {
+    public static function load(string $page , array | null $data =null) : void {
         $data && extract($data);
         // var_dump(__DIR__);
         require_once __DIR__."/../../public/templates/$page.phtml";
