b9621b10ea02f4ce3cc57ac4a007d1783e1effa2
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-11 09:57:42 -0300

    feat: criação do controller Home e config de rota.

---
+++ src/app/controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CSTSI\Dbe2\app\controllers;
+
+class HomeController extends Controller {
+
+    public function index()
+    {
+       echo "Olá Mundo!!!";
+    }
+}
--- src/config/routes.php
+++ src/config/routes.php
@@ -1,7 +1,8 @@
 <?php
 
-use CSTSI\Dbe2\app\controllers\ProdutoController;
+use CSTSI\Dbe2\app\controllers\{ProdutoController, HomeController};
 
 $routes = [
+    '/' => HomeController::class,
     'produtos' => ProdutoController::class
 ];
